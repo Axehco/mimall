2b58558c47a9b4c99fbdfff0060a819d10fc368a
Axehco <mbozhong@outlook.com>
2022-06-15 15:39:02 +0800

    项目目录结构

---
+++ src/App_跨域解决.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+import jsonp from 'jsonp'
+export default {
+  name: 'App',
+  components: {},
+  mounted() {
+    // let url = 'https://www.easy-mock.com/mook/5b012c1fe6e1035843cd3aff/mockapi/table/list'
+    // let url = 'https://www.imooc.com/activity/servicetime'
+    let url = '/api/activity/servicetime'
+    // axios.get(url).then(() => {})
+    jsonp(url, (err, res) => {
+      console.log(res);
+    })
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,  // 关闭eslint校验
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    proxy: {
+      // '/activity': {
+      //   target: 'https://www.imooc.com',
+      //   // 是否要将主机头的源点更改为目标源的URL地址 
+      //   // 默认是false：请求头中host仍然是浏览器发送过来的host
+      //   changeOrigin: true,  
+      //   pathRewrite: {
+      //     '/activity': '/activity'
+      //   }
+      // }
+      '/api': {
+        target: 'https://www.imooc.com',
+        // 是否要将主机头的源点更改为目标源的URL地址 
+        // 默认是false：请求头中host仍然是浏览器发送过来的host
+        changeOrigin: true,  
+        pathRewrite: {
+          '/api': ''
+        }
+      }
+    }
+  }
 })
