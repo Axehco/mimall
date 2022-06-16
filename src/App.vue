3c3821e6a72e786e5c641a93ede098c8570d1f66
Axehco <mbozhong@outlook.com>
2022-06-16 22:13:22 +0800

    4Mock测试

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script>
-// import storege from './storage/index.js'
 export default {
   name: 'App',
   components: {
@@ -13,25 +12,26 @@ export default {
   },
   data() {
     return {
-
+      res: {}
     }
   },
   mounted() {
-    // // 注意要先创建一个空模块，然后再往模块里面加东西
-    // storege.setItem('userinfo', {})
-    // storege.setItem('user', {})
-    // storege.setItem('age', 1, 'user')
-    // storege.setItem('name', 'zs', 'user')
-    // storege.setItem('name', 'lisi', 'userinfo')
+    // 1、本地加载请求静态json文件的形式
+    // this.axios.get('/mock/user/login.json').then((res)=>{
+    //   this.res = res;
+    // })
 
-    // storege.setItem('hobby', 'basketball')
-    // storege.setItem('age', 18)
+    // // 2、通过easymook平台
+    // // 在main.js中要将axios.defaults.baseURL = X  换成easy-mock的地址
+    // this.axios.get('/mock/user/login.json').then((res)=>{
+    //   this.res = res;
+    // })
 
-    // storege.clear('age')
-    // storege.clear('age', 'user')
-    
-    // console.log(storege.getItem('age'));
-    // console.log(storege.getItem('age', 'user'));
+    // 3、本地集成mockjs实现数据mock
+    this.axios.get('user/login.json').then((res)=>{
+      this.res = res;
+      console.log(res);
+    })
   },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,17 +4,23 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 import App from './App.vue'
-import env from './env'
+// import env from './env'
+
+// mock开关
+const mock = true
+if (mock) {
+  require('./mock/api');
+}
 
 // 根据前端的跨域方式做调整
 // 接口代理：接口域名和前端的域名是一样的，baseURL要写成 '/api/XX'
 // jsonp或cors跨域：baseURL要写成 'https://XX'
-// axios.defaults.baseURL = '/api';
+axios.defaults.baseURL = '/api';
 axios.defaults.timeout = 8000;
 
 // 这种方式是针对cors或jsonp的方式进行请求
 // 也就是说这样才去建env.js，配置里面的变量，而接口代理的方式是不需要的
-axios.defaults.baseURL = env.baseURL  
+// axios.defaults.baseURL = env.baseURL  
 
 // 接口错误拦截
 // response是axios插件封装给我们的，并不是接口返回给我们的值
