640885448baac826ff35d12b2247ff60b5f5d2db
Axehco <mbozhong@outlook.com>
2022-07-16 15:35:34 +0800

    12-1_订单父组件结构封装

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <!-- 在父页面中去统一处理title和提示信息。
+    也可以在每个子页面动态引入一次，这样就不用单独去改变变量了。 -->
+    <order-header v-bind:title="title">
+      <template v-slot:tip>
+          <span>{{tip}}</span>
+      </template>
+    </order-header>
     <router-view></router-view>
     <service-bar></service-bar>
     <nav-footer></nav-footer>
@@ -9,6 +16,7 @@
 <script>
 import ServiceBar from './../components/ServiceBar'
 import NavFooter from './../components/NavFooter'
+import orderHeader from './../components/OrderHeader.vue'
 export default {
   name: 'order',
   data() {
@@ -19,7 +27,21 @@ export default {
   },
   components: {
     ServiceBar,
-    NavFooter
+    NavFooter,
+    orderHeader
+  },
+  mounted() {
+    let path = this.$route.path;
+    if (path === '/order/confirm') {
+      this.title = '订单确认';
+      this.tip = '请认真填写收货地址';
+    } else if (path === '/order/list') {
+      this.title = '订单列表';
+      this.tip = '请谨防钓鱼链接或诈骗电话，了解更多';
+    } else if (path === '/order/pay') {
+      this.title = '订单支付';
+      this.tip = '请谨防钓鱼链接或诈骗电话，了解更多';
+    }
   }
 }
 </script>
