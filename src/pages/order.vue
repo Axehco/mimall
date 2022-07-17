f0670c0168e3b282031711c807526cc199228e57
Axehco <mbozhong@outlook.com>
2022-07-17 09:55:51 +0800

    14-2_订单列表数据渲染

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -2,11 +2,11 @@
   <div>
     <!-- 在父页面中去统一处理title和提示信息。
     也可以在每个子页面动态引入一次，这样就不用单独去改变变量了。 -->
-    <order-header v-bind:title="title">
+    <!-- <order-header v-bind:title="title">
       <template v-slot:tip>
           <span>{{tip}}</span>
       </template>
-    </order-header>
+    </order-header> -->
     <router-view></router-view>
     <service-bar></service-bar>
     <nav-footer></nav-footer>
@@ -16,7 +16,7 @@
 <script>
 import ServiceBar from './../components/ServiceBar'
 import NavFooter from './../components/NavFooter'
-import orderHeader from './../components/OrderHeader.vue'
+// import orderHeader from './../components/OrderHeader.vue'
 export default {
   name: 'order',
   data() {
@@ -28,24 +28,24 @@ export default {
   components: {
     ServiceBar,
     NavFooter,
-    orderHeader
+    // orderHeader
   },
-  mounted() {
-    let path = this.$route.path;
-    if (path === '/order/confirm') {
-      this.title = '订单确认';
-      this.tip = '请认真填写收货地址';
-    } else if (path === '/order/list') {
-      this.title = '订单列表';
-      this.tip = '请谨防钓鱼链接或诈骗电话，了解更多';
-    } else if (path === '/order/pay') {
-      this.title = '订单支付';
-      this.tip = '请谨防钓鱼链接或诈骗电话，了解更多';
-    } else {
-      this.title = '订单支付';
-      this.tip = '请谨防钓鱼链接或诈骗电话，了解更多';
-    }
-  }
+  // mounted() {
+  //   let path = this.$route.path;
+  //   if (path === '/order/confirm') {
+  //     this.title = '订单确认';
+  //     this.tip = '请认真填写收货地址';
+  //   } else if (path === '/order/list') {
+  //     this.title = '订单列表';
+  //     this.tip = '请谨防钓鱼链接或诈骗电话，了解更多';
+  //   } else if (path === '/order/pay') {
+  //     this.title = '订单支付';
+  //     this.tip = '请谨防钓鱼链接或诈骗电话，了解更多';
+  //   } else {
+  //     this.title = '订单支付';
+  //     this.tip = '请谨防钓鱼链接或诈骗电话，了解更多';
+  //   }
+  // }
 }
 </script>
 
